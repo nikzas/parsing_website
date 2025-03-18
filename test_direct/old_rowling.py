bba4a1ecb1352bbd1cdcb1d78b362215cba15746
Nikita Zasukin <nikudabeznego@gmail.com>
2025-03-18 15:53:05 +0300

    обновил функцию для чтения из папки и сохранения в папку

--- test_direct/old_rowling.py
+++ test_direct/old_rowling.py
@@ -1,48 +1,60 @@
 import pandas as pd
 import numpy as np
+import os
+import glob
+
+# Путь к папке с CSV файлами
+input_folder = r'D:\ProjectPython\parsing_website\test_direct\csv_results\*.csv'
+output_folder = r'D:\ProjectPython\parsing_website\test_direct\result_ready'
 
 
-df = pd.read_csv(r'D:\ProjectPython\parsing_website\test_direct\csv_results\.csv')
+# Получаем список всех CSV файлов в папке
+csv_files = glob.glob(input_folder)
 
-data = df.drop(columns='Time').values
-time_column = df['Time'].values
+for file in csv_files:
+    df = pd.read_csv(file)
 
-new_numbers = []
-time_new_numbers = []
+    data = df.drop(columns='Time').values
+    time_column = df['Time'].values
 
-# Добавляем первое число из первой строки
-first_number = data[0][0]  # Первое число из первой строки
-new_numbers.append(first_number)
-time_new_numbers.append(time_column[0])  # Время для первого числа
+    new_numbers = []
+    time_new_numbers = []
 
-previous_row = data[0]  # Устанавливаем предыдущую строку как первую строку
+    # Добавляем первое число из первой строки
+    first_number = data[0][0]  # Первое число из первой строки
+    new_numbers.append(first_number)
+    time_new_numbers.append(time_column[0])  # Время для первого числа
 
-for index in range(1, len(data)):  # Начинаем с 1, так как 0 уже обработано
-    current_row = data[index]
-    # Находим смещение: ищем, с какого индекса текущая строка совпадает с предыдущей
-    match_start = None
-    for i in range(len(current_row)):
-        # Проверяем совпадение оставшихся элементов
-        if np.array_equal(current_row[i:], previous_row[:len(current_row) - i]):
-            match_start = i
-            break
-    if match_start is not None:
-        # Получаем сегмент новых чисел
-        segment = current_row[:match_start].tolist()
-        new_numbers.extend(segment[::-1])
-        # Добавляем соответствующее время
-        time_new_numbers.extend([time_column[index]] * len(segment[::-1]))
+    previous_row = data[0]  # Устанавливаем предыдущую строку как первую строку
 
-    previous_row = current_row  # Обновляем предыдущую строку
+    for index in range(1, len(data)):  # Начинаем с 1, так как 0 уже обработано
+        current_row = data[index]
+        # Находим смещение: ищем, с какого индекса текущая строка совпадает с предыдущей
+        match_start = None
+        for i in range(len(current_row)):
+            # Проверяем совпадение оставшихся элементов
+            if np.array_equal(current_row[i:], previous_row[:len(current_row) - i]):
+                match_start = i
+                break
+        if match_start is not None:
+            # Получаем сегмент новых чисел
+            segment = current_row[:match_start].tolist()
+            new_numbers.extend(segment[::-1])
+            # Добавляем соответствующее время
+            time_new_numbers.extend([time_column[index]] * len(segment[::-1]))
 
-# Создаем новый DataFrame с временем и новыми числами
-result_df = pd.DataFrame({
-    'Time': time_new_numbers,
-    'New Numbers': new_numbers
-})
+        previous_row = current_row  # Обновляем предыдущую строку
 
-print("Новые числа:", new_numbers)
-print("Количество новых чисел:", len(new_numbers))
+    # Создаем новый DataFrame с временем и новыми числами
+    result_df = pd.DataFrame({
+        'Time': time_new_numbers,
+        'New Numbers': new_numbers
+    })
 
-# Запись результатов в файл
-result_df.to_csv(r'D:\ProjectPython\parsing_website\test_direct\results\.csv', index=False)
+    # Формируем имя выходного файла
+    output_file = os.path.join(output_folder, os.path.basename(file))
+
+    # Запись результатов в файл
+    result_df.to_csv(output_file, index=False)
+
+    print(f"Обработан файл: {file}, сохранен как: {output_file}")
